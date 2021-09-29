0955aaf85744a7bbabd8173b2867e2b3f6fd7fb1
nickremouchamps <91624396+nickremouchamps@users.noreply.github.com>
2021-09-29 19:37:25 +0200

    Update import_raman.py

--- Code/import_raman.py
+++ Code/import_raman.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
 for i in range(1,11):
-    path = '/content/drive/MyDrive/Preprocessing/Raman/Batch' +str(i) +'_region.csv'
+    path = '/content/colabdata/Raman/Batch' +str(i) +'_region.csv'
     with open(path, newline='') as csvfile:
         df = pd.read_csv(csvfile, dtype='float', na_values=['??', 'None', 'NaN'])
 
     df = df.iloc[11:][:]
-    path = '/content/drive/MyDrive/Preprocessing/Raman/coeff' +str(i) +'.pkl'
+    path = '/content/colabdata/Preprocessing/Raman/coeff' +str(i) +'.pkl'
     df.to_pickle(path)
